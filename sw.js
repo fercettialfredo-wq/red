0f0549a4706b3c8217c9d166b54a84042b714f82
fercettialfredo-wq <fercettialfredo@gmail.com>
2026-01-20 16:11:55 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // CAMBIO 1: Subimos la versión a 'v2' para obligar al navegador a borrar lo viejo
-const CACHE_NAME = 'ravens-access-v8';
+const CACHE_NAME = 'ravens-access-v9';
 
 const urlsToCache = [
   './',
